92a2df55c97aa2e462d988defe5ceba19af1be74
jduigou <johanna.pro@protonmail.com>
2018-03-20 22:45:50 +0100

    :rocket: add a manual action to change the WS entrypoint

--- app/src/main/java/com/breizhcamp/ticket/activities/MainActivity.kt
+++ app/src/main/java/com/breizhcamp/ticket/activities/MainActivity.kt
@@ -1,40 +1,63 @@
 package com.breizhcamp.ticket.activities
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.WindowManager
+import com.breizhcamp.ticket.AppConfig
 import com.breizhcamp.ticket.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ApplySharedPref")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val preferences = getSharedPreferences(AppConfig.SHARE_PREFERENCIES_NAME, Context.MODE_PRIVATE)
 
         // making toolbar transparent
         transparentToolbar()
 
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.btn_scan).setOnClickListener {  startActivity(Intent(this@MainActivity, ScanActivity::class.java)) }
+        scanButton.setOnClickListener {
+            if(!urlTextInputLayout.editText!!.text.isNullOrEmpty()) {
+                val editor = preferences.edit()
+                editor.putString (AppConfig.WEBSERVICE_ENTRYPOINT, urlEditText.editableText.toString())
+                editor.commit ()
+                startActivity(Intent(this@MainActivity, ScanActivity::class.java))
 
+            }else {
+                urlTextInputLayout.error = this.resources.getString(R.string.error_websites_entry)
+            }
+        }
     }
 
     private fun transparentToolbar() {
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+        if (atLeastAPI(19) && atMostAPI(21)) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
         }
-        if (Build.VERSION.SDK_INT >= 19) {
+        if (atLeastAPI(19)) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
-        if (Build.VERSION.SDK_INT >= 21) {
+        if (atLeastAPI(21)) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
             window.statusBarColor = Color.TRANSPARENT
         }
+    }
+
+    private fun atMostAPI(api: Int): Boolean {
+        return api > android.os.Build.VERSION.SDK_INT
+    }
+
+    private fun atLeastAPI(api: Int): Boolean {
+        return api <= android.os.Build.VERSION.SDK_INT
     }
 
     private fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
